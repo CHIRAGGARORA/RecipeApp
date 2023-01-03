e21a647e3cd93296be7c11ea2579a5abc172c251
Chirag Arora <arora2002chirag@gmail.com>
2023-01-03 14:33:59 +0530

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by chirag arora on 03/01/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
